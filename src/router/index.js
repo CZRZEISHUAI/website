2c9f01ee0be2e4e31cc40d885688e75634ec2def
CZRZEISHUAI <46983166+CZRZEISHUAI@users.noreply.github.com>
2023-12-01 16:28:02 +0800

    项目模板

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Index',
+        meta: {
+            title: '首页',
+            keepAlive: true,
+            requireAuth: true,
+        },
+        component: () => import('@/pages/index.vue'),
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+export default router;
